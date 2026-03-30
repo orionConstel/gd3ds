81e306ca03df1e174bd5211d0812017db2712517
AleFunky <alefunkyextra@gmail.com>
2026-03-30 21:19:30 +0200

    Shrink orb collide effect thickness

--- source/particles/circles.c
+++ source/particles/circles.c
@@ -66,7 +66,7 @@ const UseEffectDefinition orb_collide_effect = {
     .end_opacity_ease = EASE_OUT,
     .start_rad_ease = EASE_OUT,
     .end_rad_ease = EASE_OUT,
-    .line_thickness = 2.f
+    .line_thickness = 1.f
 };
 UseEffect use_effects_top[MAX_USE_EFFECTS];
 UseEffect use_effects_bot[MAX_USE_EFFECTS];
